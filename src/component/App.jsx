552d9e2fa480489d877c89416055418773887a50
SergeiKrutii <106181150+SergeiKrutii@users.noreply.github.com>
2023-03-11 22:29:54 +0200

    and second step

---
+++ src/component/App.jsx
@@ -0,0 +1,12 @@
+import Feedback from "./Feedback/Feedback";
+// import Phonebook from "./Phonebook/Phonebook";
+
+export const App = () => {
+  return (
+    <>
+      <Feedback />
+      {/* <Phonebook /> */}
+    </>
+  );
+};
+export default App;
